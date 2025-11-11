1425596f67a84dd2a1961f2b09c48f41cf74f1f8
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2025-11-11 15:17:44 -0300

    Reading file and developing the editing process

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 # include <stdio.h>
 
 void main(int argc, char** argv) {
+    // Reading file, using pointers and buffers
+    // Opening text file for reading
     FILE* f = fopen(argv[1], "r");
     char buffer[1024] = {0};
     fread(buffer, 1024, 1, f);
     fclose(f);
-    
+
+    // Once the contents are in the buffer, we start the editing process
+        // What line the user wants to edit
+        int current_line = 0;
+        printf("Contents:\n%s\n", buffer);
+        scanf("%d", &current_line);
+        edit_line(current_line, buffer)
 }
