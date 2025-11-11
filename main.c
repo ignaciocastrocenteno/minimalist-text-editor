fe3048be158c8ef8222b26928324474c4cd8f26c
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2025-11-11 15:31:29 -0300

    Creating the edit_line() to manage the text editor logic

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 void main(int argc, char** argv) {
     // Reading file, using pointers and buffers
@@ -14,4 +15,27 @@ void main(int argc, char** argv) {
         printf("Contents:\n%s\n", buffer);
         scanf("%d", &current_line);
         edit_line(current_line, buffer)
+        
+        // Opening the file again but in write mode
+        f - fopen(argv[1], "w");
+        
+        // Getting a pointer to write to the file
+        fwrite(buffer, strlen(buffer), 1, f);
+        fclose(f);
+}
+
+void edit_line(char* buffer, int current_line) {
+    // Locate the line we want to go to in the buffer
+    for (int i = 0; i < current_line; i++) {
+        // Adding +1 positioning ahead of the new text line
+        buffer = strchr(buffer, '\n') + 1;
+    }
+
+    char* line_end = strchr(buffer, '\n');
+    char saved[1024] = {0};
+    // Copying the line end
+    strcpy(saved, line_end);
+    scanf("%s", buffer);
+    // Getting the length of the text line, the user entered
+    strcpy(buffer + strlen(buffer), saved);
 }
